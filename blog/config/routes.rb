7a4a3f3f7584a4d8a9fda67f13356fd7eab5bd04
Unknown <stanshalomchin@yahoo.com>
2017-02-07 13:42:00 +0800

    Generate Posts controller, Create new.html.erb

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root "welcome#index"
   resources :posts
 
+  # get 'posts/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
