e78aa3d08f2e9e76e9bedde3a5d83239efe03172
Unknown <stanshalomchin@yahoo.com>
2017-02-07 14:49:42 +0800

    Save new post, Create save function, Create view.html.erb

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,8 +1,28 @@
 class PostsController < ApplicationController
-	def new
+	def index
+   @posts = Post.all
+   end 
+
+  def new
 	end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 
 	def create
     render plain: params[:post].inspect
+  end
+
+  def create
+  @post = Post.new(post_params)
+ 
+  @post.save
+  redirect_to @post
+
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :date, :text)
   end
 end
