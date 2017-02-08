bfe801218b66ce5437da76f8e08b3de160e95ee8
Unknown <stanshalomchin@yahoo.com>
2017-02-08 18:36:30 +0800

    Add validations on form so that it is clearer
    http://railscasts.com/episodes/263-client-side-validations?view=asciicast

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ApplicationRecord
 	validates :title, presence: true,
                     length: { in: 15..75 } 
-    validates_uniqueness_of :title, :case_sensitive => false
-                    
+  validates_uniqueness_of :title, :case_sensitive => false
+  validates :author, presence: true,
+                    length: { minimum: 5 }
+
 end
+
 
 	
