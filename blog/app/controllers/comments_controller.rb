77ef7c4edac5dd58af1376701b062d1768aca679
Unknown <stanshalomchin@yahoo.com>
2017-02-09 16:25:42 +0800

    Add delete function to comments of blog

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -3,8 +3,16 @@ def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
-  end
- 
+ end
+
+ def destroy
+ 	@post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    
+    redirect_to post_path(@post)
+ end
+
   private
     def comment_params
       params.require(:comment).permit(:comment, :name, :email)
